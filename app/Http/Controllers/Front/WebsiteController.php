9d49f5cb4b1d4ab96c5bc19bbfa93b8be0ab4fa3
Amit Kumar Sutradhar <amit596450@gmail.com>
2023-09-29 13:02:26 +0600

    Frontend Mastering Done

---
+++ app/Http/Controllers/Front/WebsiteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WebsiteController extends Controller
+{
+    public function home(){
+        return view('frontend.home.home');
+    }
+
+    public function categoryBlog(){
+        return view('frontend.blogs.category-blog');
+    }
+    public function blogDetails(){
+        return view('frontend.blogs.blog-details');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\WebsiteController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WebsiteController::class,'home'])->name('home');
+Route::get('/category-blog', [WebsiteController::class,'categoryBlog'])->name('category-blog');
+Route::get('/blog-details', [WebsiteController::class,'blogDetails'])->name('blog-details');
 
 Route::middleware([
     'auth:sanctum',
