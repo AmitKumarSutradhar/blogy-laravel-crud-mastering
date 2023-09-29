c39c4b4c88ec1d729c85bfec4b95cbbaedeacbd7
Amit Kumar Sutradhar <amit596450@gmail.com>
2023-09-30 02:01:35 +0600

    Create Blog Category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Front\WebsiteController;
+use App\Http\Controllers\Backend\BlogCategoryController;
 
 
 Route::get('/', [WebsiteController::class,'home'])->name('home');
@@ -14,4 +15,11 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', [WebsiteController::class,'dashboard'])->name('dashboard');
+
+    Route::get('/create-blog-categories',[BlogCategoryController::class,'create'])->name('blog-categories.create');
+    Route::post('/store-blog-categories',[BlogCategoryController::class,'store'])->name('blog-categories.store');
+    Route::get('/manage-blog-categories',[BlogCategoryController::class,'manage'])->name('blog-categories.manage');
+    Route::get('/edit-blog-categories',[BlogCategoryController::class,'edit'])->name('blog-categories.edit');
+    Route::post('/update-blog-categories',[BlogCategoryController::class,'update'])->name('blog-categories.update');
+    Route::get('/delete-blog-categories',[BlogCategoryController::class,'delete'])->name('blog-categories.delete');
 });
